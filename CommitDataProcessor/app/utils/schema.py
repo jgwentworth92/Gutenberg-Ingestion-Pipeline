caab1ceae029375b2c6576f47460aae6d1669424
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2024-05-01 16:41:57 -0400

    added pydantic schema microservice 2

---
+++ CommitDataProcessor/app/utils/schema.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel
+
+class FileInfo(BaseModel):
+    filename: str
+    status: str
+    additions: int
+    deletions: int
+    changes: int
+    patch: Optional[str] = None
+
+class CommitData(BaseModel):
+    author: str
+    message: str
+    date: str
+    url: str
+    repo_name:str
+    commit_id: str
+    files: List[FileInfo]
